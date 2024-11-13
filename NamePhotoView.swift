4f598ab3af26637cdefa1715d3093b4a46a01980
DanutPopa <popa.ion.danut@gmail.com>
2024-11-13 18:24:48 +0200

    Name the photo when a new photo is imported.

---
+++ NamePhotoView.swift
@@ -0,0 +1,18 @@
+//
+//  NamePhotoView.swift
+//  RememberMe
+//
+//  Created by Danut Popa on 13.11.2024.
+//
+
+import SwiftUI
+
+struct NamePhotoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NamePhotoView()
+}
