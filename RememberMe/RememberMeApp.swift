3ae6d8f938b3fa5b36f2096744dadd6cdaf9c8f9
DanutPopa <popa.ion.danut@gmail.com>
2024-11-13 15:07:21 +0200

    Initial Commit

---
+++ RememberMe/RememberMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RememberMeApp.swift
+//  RememberMe
+//
+//  Created by Danut Popa on 13.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct RememberMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
