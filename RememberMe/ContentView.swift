14aa256b333ea0bb242eb7f1c8ea22ed5a236363
DanutPopa <popa.ion.danut@gmail.com>
2024-11-13 16:19:49 +0200

    Import photo from photo library.

--- RememberMe/ContentView.swift
+++ RememberMe/ContentView.swift
@@ -6,16 +6,27 @@
 //
 
 import SwiftUI
+import PhotosUI
 
 struct ContentView: View {
+    @State private var pickerItem: PhotosPickerItem?
+    @State private var selectedImage: Image?
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            PhotosPicker(selection: $pickerItem, matching: .all(of: [.images, .not(.screenshots)])) {
+                Label("Select a picture", systemImage: "photo")
+            }
+            
+            selectedImage?
+                .resizable()
+                .scaledToFit()
         }
-        .padding()
+        .onChange(of: pickerItem) {
+            Task {
+                selectedImage = try await pickerItem?.loadTransferable(type: Image.self)
+            }
+        }
     }
 }
 
